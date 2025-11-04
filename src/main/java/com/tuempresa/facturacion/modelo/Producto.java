d292d9e9e61f7bf5f0eb9c00b634035f6b09d351
Emma Serrano <emmaceciliaserranourroz@gmail.com>
2025-11-03 21:06:43 -0600

    Leccion3 Modelo de dominio pte2.

--- src/main/java/com/tuempresa/facturacion/modelo/Producto.java
+++ src/main/java/com/tuempresa/facturacion/modelo/Producto.java
@@ -31,13 +31,5 @@ public class Producto {
     @TextArea // Esto es para un texto grande, se usará un área de texto o equivalente
     String observaciones;
 
-    @Money // La propiedad precio se usa para almacenar dinero
-    BigDecimal precio; // BigDecimal se suele usar para dinero
 
-    @Files // Una galería de fotos completa está disponible
-    @Column(length=32) // La cadena de 32 de longitud es para almacenar la clave de la galería
-    String fotos;
-
-    @TextArea // Esto es para un texto grande, se usará un área de texto o equivalente
-    String observaciones;
 }
